9aba7c3dccd2258b80e818f089d3069b1494e183
bindu-666 <113664218+bindu-666@users.noreply.github.com>
2024-07-16 10:48:00 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,67 +1,46 @@
 'use strict';
 
-
-
-
-
-
-
-
-
 // mobile menu variables
 const mobileMenuOpenBtn = document.querySelectorAll('[data-mobile-menu-open-btn]');
 const mobileMenu = document.querySelectorAll('[data-mobile-menu]');
 const mobileMenuCloseBtn = document.querySelectorAll('[data-mobile-menu-close-btn]');
 const overlay = document.querySelector('[data-overlay]');
 
-for (let i = 0; i < mobileMenuOpenBtn.length; i++) {
-
-  // mobile menu function
-  const mobileMenuCloseFunc = function () {
-    mobileMenu[i].classList.remove('active');
+const mobileMenuCloseFunc = function (index) {
+  return function() {
+    mobileMenu[index].classList.remove('active');
     overlay.classList.remove('active');
-  }
+  };
+};
 
+for (let i = 0; i < mobileMenuOpenBtn.length; i++) {
   mobileMenuOpenBtn[i].addEventListener('click', function () {
     mobileMenu[i].classList.add('active');
     overlay.classList.add('active');
   });
 
-  mobileMenuCloseBtn[i].addEventListener('click', mobileMenuCloseFunc);
-  overlay.addEventListener('click', mobileMenuCloseFunc);
-
+  mobileMenuCloseBtn[i].addEventListener('click', mobileMenuCloseFunc(i));
+  overlay.addEventListener('click', mobileMenuCloseFunc(i));
 }
-
-
-
-
 
 // accordion variables
 const accordionBtn = document.querySelectorAll('[data-accordion-btn]');
 const accordion = document.querySelectorAll('[data-accordion]');
 
 for (let i = 0; i < accordionBtn.length; i++) {
-
   accordionBtn[i].addEventListener('click', function () {
-
     const clickedBtn = this.nextElementSibling.classList.contains('active');
 
-    for (let i = 0; i < accordion.length; i++) {
-
+    for (let j = 0; j < accordion.length; j++) {
       if (clickedBtn) break;
 
-      if (accordion[i].classList.contains('active')) {
-
-        accordion[i].classList.remove('active');
-        accordionBtn[i].classList.remove('active');
-
+      if (accordion[j].classList.contains('active')) {
+        accordion[j].classList.remove('active');
+        accordionBtn[j].classList.remove('active');
       }
-
     }
 
     this.nextElementSibling.classList.toggle('active');
     this.classList.toggle('active');
-
   });
-
 }
